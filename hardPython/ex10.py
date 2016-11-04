6255af4dbfe9e2be6f5d53262d201938eb91b90a
= <=>
2016-11-04 14:41:25 +0800

    change the dir

---
+++ hardPython/ex10.py
@@ -0,0 +1,9 @@
+# --coding:utf-8 --
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So,you're %r old,%r tall and %r heave."%(age,height,weight)
