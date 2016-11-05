e868a485712006bbc2f1de7c06667b52e7f661a2
= <=>
2016-11-05 06:27:39 -0700

    分割字符串，读取写入文件联系

---
+++ basePython/split.py
@@ -0,0 +1,13 @@
+#-*-conding:utf-8-*-
+import string
+file_input_object = open('u.item')
+file_output_object = open('sep.item','w+')
+
+for one_text in  file_input_object:
+	one_text_split = one_text.split('|')[6:24]
+	one_text_split_str = ",".join(one_text_split)
+	file_output_object.write(one_text_split_str)
+
+#one = file_input_object.readline()
+#one_split = one.split('|')
+#print one_split[6:24]
