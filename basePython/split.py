b357ceeb2dedbfa26618459a386b1712e1a52d42
= <=>
2016-11-05 06:33:33 -0700

    bug

--- basePython/split.py
+++ basePython/split.py
@@ -4,7 +4,7 @@ file_input_object = open('u.item')
 file_output_object = open('sep.item','w+')
 
 for one_text in  file_input_object:
-	one_text_split = one_text.split('|')[6:24]
+	one_text_split = one_text.split('|')[5:24]
 	one_text_split_str = ",".join(one_text_split)
 	file_output_object.write(one_text_split_str)
 
