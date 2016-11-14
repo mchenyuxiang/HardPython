05b21fa2ac98655400341084c8262a9791a9d76d
mchenyuxiang <csyxchen@qq.com>
2016-11-14 17:37:09 +0800

    多关键字排名取得。另外计算前50名的排名

--- spiderPython/combaiduspider/spider_main.py
+++ spiderPython/combaiduspider/spider_main.py
@@ -1,5 +1,6 @@
 # -*-coding:utf-8-*-
 import urllib
+import re
 
 from combaiduspider import html_downloader
 from combaiduspider import html_parser
@@ -12,27 +13,57 @@ class SpiderMain(object):
         self.parser = html_parser.HtmlParser()
         # self.outputer = html_outputer.HtmlOutputer()
 
-    def craw(self,root_url,root_user_url):
-        # 得到:关键词列表
-        # new_keyword = self.keyword.get_new_keyword()
-        html_cont = self.downloader.download(root_url)
-        # 得出网址
-        new_data = self.parser.paser(html_cont)
-        # self.url.add_new_url(new_urls)
-        # self.outputer.collect_data(new_data)
-        # self.output.output_html()
-        for name in new_data:
-            print name.get_text()
+    def craw(self,root_url,root_user_url,root_name_all):
+        for root_name_single in root_name_all:
+            root_name = quote_plus(root_name_single)
+            # 得到:关键词列表
+            root_pn = 0
+            domain_rank = root_pn / 10 - 1
+            for root_pn in range(0, 51 ,10):
+                html_wd_pn = "wd=%s&pn=%d" % (root_name,root_pn)
+                html_url = root_url+html_wd_pn
+                # print html_url
+                html_cont = self.downloader.download(html_url)
+                # 得出网址
+                new_data = self.parser.paser(html_cont)
+                # 得到客户的域名地址
+                user_url_data = root_user_url.split(".")
+                leng_url = len(user_url_data)
+                if leng_url == 1:
+                    user_domain = user_url_data
+                elif leng_url > 1:
+                    user_domain = user_url_data[1]
+                else:
+                    user_domain = '请输入域名'
+                # print type(user_url_data)
+                # print user_domain
+                # 查询用户网址是否在当前页面，如果不在则翻页，最多查询5页内容
+                for name in new_data:
+                    pattern = re.compile(r'%s'%user_domain)
+                    result1 = re.search(pattern, name.get_text())
+                    if result1:
+                        domain_rank = new_data.index(name)+1
+                        domain_rank = root_pn + domain_rank
+                        # print domain_rank
+                        break
+                    # print name.get_text()
+                # print domain_rank
+                if domain_rank != -1:
+                    break
 
+            if domain_rank != -1:
+                print "关键词：%s\t百度排名为： %d" % (root_name_single,domain_rank)
+            else:
+                print "关键词：%s\t百度排名50名之外" % root_name_single
 
 if __name__ == '__main__':
-    root_name = '畜牧蚊香'
+    root_name = ['长沙消防工程公司','消防工程安装公司','长沙安防工程','长沙智能化弱电工程']
     # root_name = root_name.encode('utf-8')
-    root_name = quote_plus(root_name)
-    root_user_url = 'www.wuzhouwenxiang.com'
+    root_user_url = 'www.hndtai.com'
+    # root_user_url = 'http://www.seoai.cn/'
     # print root_name
-    root_pd = 0
-    root_url = "http://www.baidu.com/s?wd=%s&pn=%d" % (root_name,root_pd)
-    print root_url
+    # root_pn = 0i
+    root_url = "http://www.baidu.com/s?"
+    # print root_url
     obj_spider = SpiderMain()
-    obj_spider.craw(root_url,root_user_url)
+    obj_spider.craw(root_url,root_user_url,root_name)
