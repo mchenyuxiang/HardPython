49b45340969a3e517c812445f55f4c0a605005e3
mchenyuxiang <csyxchen@qq.com>
2017-02-19 07:25:12 -0800

    添加useragent 头

--- scarpySpider/scarpySpider/settings.py
+++ scarpySpider/scarpySpider/settings.py
@@ -53,9 +53,11 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+DOWNLOADER_MIDDLEWARES = {
 #    'scarpySpider.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+   'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+   'scarpySpider.middlewares.RotateUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
