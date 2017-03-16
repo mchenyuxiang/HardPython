92b15e9b1a479757d16104a8bf76f050ba2c1439
mchenyuxiang <csyxchen@qq.com>
2017-03-16 23:49:42 +0800

    手机百度

--- scarpySpider/main.py
+++ scarpySpider/main.py
@@ -1,7 +1,12 @@
 # coding:utf-8
 
-from scrapy import cmdline
+# from scrapy import cmdline
+import os
+import sys
 
 # cmdline.execute("scrapy crawl quotes".split())
-cmdline.execute("scrapy crawl baidu_rank".split())
+# cmdline.execute("scrapy crawl baidu_mobile_rank".split())
+# cmdline.execute("scrapy crawl baidu_rank".split())
 # cmdline.execute("scrapy crawl author".split())
+os.system("scrapy crawl baidu_rank")
+os.system("scrapy crawl baidu_mobile_rank")
